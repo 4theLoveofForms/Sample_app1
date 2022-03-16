14910ec62e07a9f942da84e0632ccc2adb9c35ff
4theLoveofForms <tangledrootsinfo@gmail.com>
2022-03-16 15:00:23 +0000

    you can call the api.  curl --user "example@railstutorial.org:foobar" http://localhost:3000/api/v1/microposts

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :microposts,  only: [:create, :destroy]
-   
+  namespace :api do
+    namespace :v1 do
+      resources :microposts, only: [:index, :show, :create, :destroy]
+    end
+  end 
 end
