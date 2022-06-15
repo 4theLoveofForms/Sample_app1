135209254281f26502c787ed8c7e53e8ef221f58
4theLoveofForms <tangledrootsinfo@gmail.com>
2022-06-15 11:27:51 +0100

    Changed migration to be reversable and to include extras

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
   has_many :microposts, dependent: :destroy #I prob want to get rid of this second argument later
       before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
    validates :email, presence: true, length: { maximum: 255 },
-                           format: { with: VALID_EMAIL_REGEX }, 
+                           format: { with: VALID_EMAIL_REGEX },
                            uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true 
-  
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
    # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
